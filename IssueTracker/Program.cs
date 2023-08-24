e140411e69e863e161934d3e8fa4b201bd3798f1
Burak Karataş <burakkaratasz@outlook.com>
2023-08-24 13:12:59 +0300

    serilog added

--- IssueTracker/Program.cs
+++ IssueTracker/Program.cs
@@ -1,15 +1,29 @@
-using IssueTracker.Data;
+﻿using IssueTracker.Data;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
 
 namespace IssueTracker
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseSerilog((context, config) =>    //loglama
+            {
+                config.WriteTo.File("log.txt");
+                
+            });
+
             // Add services to the container.
+
             builder.Services.AddRazorPages();
             builder.Services.AddDbContext<IssueDbContext>(o => o.UseSqlite("filename=Data/Database/Issue.db"));
 
